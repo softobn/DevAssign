91e7914d11f50817d9c84dfb06d21281bfb49fe6
Zisan05 <mdzisanislam69@gmail.com>
2024-06-06 02:47:46 +0800

    Comment section hardcoded part done

--- src/Components/ProjectTaskPage/ProjectTaskPage.jsx
+++ src/Components/ProjectTaskPage/ProjectTaskPage.jsx
@@ -3,6 +3,7 @@ import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { FiMinimize, FiPlus } from "react-icons/fi";
 import Swal from "sweetalert2";
 import { MdOutlineMarkEmailUnread } from "react-icons/md";
+import { IoCloseCircleSharp } from "react-icons/io5";
 
 const ProjectTaskPage = () => {
   const [allProjectInfo, setAllProjectInfo] = useState([]);
@@ -50,14 +51,7 @@ const ProjectTaskPage = () => {
       .then((res) => res.json())
       .then((data) => setTaskList(data));
   };
-  // useEffect(() => {
-  //   // get and set all task list by using current project id.
-  //   fetch(
-  //     `https://softobn.pythonanywhere.com/api/user/task-list/?project_id=${id}`
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => setTaskList(data));
-  // }, [id]);
+
   // projects-> find the current task id.
   const projects = taskList?.find((task) => task?.project);
   const devProjectIds = {
@@ -85,24 +79,7 @@ const ProjectTaskPage = () => {
 
     console.log(newtok);
 
-    // (async () => {
-    //   try {
-    //     const response = await fetch(
-    //       `https://softobn.pythonanywhere.com/api/user/subtask-list/?task_id=${id}`,
-    //       {
-    //         method: "GET",
-    //         headers: {
-    //           "Content-Type": "application/json",
-    //           Authorization: `Bearer ${newtok}`,
-    //         },
-    //       }
-    //     );
-    //     const data = await response.json();
-    //     console.log(data);
-    //   } catch (error) {
-    //     console.error("Error:", error);
-    //   }
-    // })();
+
 
     fetch(
       `https://softobn.pythonanywhere.com/api/user/subtask-list/?task_id=${id}`,
@@ -225,162 +202,14 @@ const ProjectTaskPage = () => {
       console.error("Error:", error);
     }
   };
-  // const handleMarkStatus = async (id) => {
-  //   // console.log(id);
-  //   fetch(`https://softobn.pythonanywhere.com/api/user/refresh/`, {
-  //     method: "POST",
-  //     credentials: "include",
-  //     headers: {
-  //       "content-type": "application/json",
-  //     },
-  //     body: JSON.stringify(token),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setNewtok(data.access);
-  //     });
 
-  //   console.log("newtok", newtok);
-  //   try {
-  //     const status = {
-  //       task_id: id,
-  //     };
-  //     console.log("status", status);
-  //     const response = await fetch(
-  //       "https://softobn.pythonanywhere.com/api/developer/task-mark/",
-  //       {
-  //         method: "PATCH",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           Authorization: `Bearer ${newtok}`,
-  //         },
-  //         body: JSON.stringify(status),
-  //       }
-  //     );
-  //     const data = await response.json();
-  //     console.log(data);
+// Comment Section
 
-  //     if (data === "success") {
-  //       Swal.fire({
-  //         title: "Successfully ",
-  //         text: "You successfully create a project",
-  //         icon: "success",
-  //       });
-  //       //   form.reset();
+const [isComment,setisComment] = useState(false);
 
-  //       // navigate(location?.state ? location?.state.pathname : "/");
-  //     }
 
-  //     if (data === "unsuccess") {
-  //       Swal.fire({
-  //         title: "Error",
-  //         text: "Please enter the information correctly !",
-  //         icon: "error",
-  //       });
-  //     }
-  //   } catch (error) {
-  //     console.error("Error:", error);
-  //   }
-  // };
-  // const handleMarkStatus = async (id) => {
-  //   console.log(id);
-  //   fetch(`https://softobn.pythonanywhere.com/api/user/refresh/`, {
-  //     method: "POST",
-  //     credentials: "include",
-  //     headers: {
-  //       "content-type": "application/json",
-  //     },
-  //     body: JSON.stringify(token),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setNewtok(data.access);
-  //     });
 
-  //   console.log("newtok", newtok);
-  //   (async () => {
-  //     const status = {
-  //       task_id: id,
-  //     };
-  //     console.log("status", status);
-  //     console.log(accessToken);
-  //     try {
-  //       const response = await fetch(
-  //         "https://softobn.pythonanywhere.com/api/developer/task-mark/",
-  //         {
-  //           method: "PATCH",
-  //           headers: {
-  //             "Content-Type": "application/json",
-  //             Authorization: `Bearer ${accessToken}`,
-  //           },
-  //           body: JSON.stringify(status),
-  //         }
-  //       );
-  //       const data = await response.json();
-  //       console.log(data);
-  //       console.log(newtok);
-
-  //       if (data === "success") {
-  //         Swal.fire({
-  //           title: "Successfully ",
-  //           text: "Task Added successfully..",
-  //           icon: "success",
-  //         });
-  //         navigate(location?.state ? location?.state.pathname : "/");
-  //       }
-
-  //       if (data === "unsuccess") {
-  //         Swal.fire({
-  //           title: "Error",
-  //           text: "Please enter the information correctly !",
-  //           icon: "error",
-  //         });
-  //       }
-  //     } catch (error) {
-  //       console.error("Error:", error);
-  //     }
-  //   })();
-  //   // try {
-  //   //   const status = {
-  //   //     task_id: id,
-  //   //   };
-  //   //   console.log("status", status);
-  //   //   const response = await fetch(
-  //   //     "https://softobn.pythonanywhere.com/api/developer/task-mark/",
-  //   //     {
-  //   //       method: "PATCH",
-  //   //       headers: {
-  //   //         "Content-Type": "application/json",
-  //   //         Authorization: `Bearer ${newtok}`,
-  //   //       },
-  //   //       body: JSON.stringify(status),
-  //   //     }
-  //   //   );
-  //   //   const data = await response.json();
-  //   //   console.log(data);
-
-  //   //   if (data === "success") {
-  //   //     Swal.fire({
-  //   //       title: "Successfully ",
-  //   //       text: "successfully marked",
-  //   //       icon: "success",
-  //   //     });
-  //   //     //   form.reset();
-
-  //   //     // navigate(location?.state ? location?.state.pathname : "/");
-  //   //   }
-
-  //   //   if (data === "unsuccess") {
-  //   //     Swal.fire({
-  //   //       title: "Error",
-  //   //       text: "Please enter the information correctly !",
-  //   //       icon: "error",
-  //   //     });
-  //   //   }
-  //   // } catch (error) {
-  //   //   console.error("Error:", error);
-  //   // }
-  // };
+  
   return (
     <div className="w-full md:max-h-[100vh] overflow-hidden">
       <div className="overflow-auto">
@@ -404,29 +233,13 @@ const ProjectTaskPage = () => {
           <p>Deadline:{projectInfo?.deadline}</p>
           <p>Requirements: {projectInfo?.requirements}</p>
         </div>
-        <div className="overflow-x-auto overflow-auto min-h-[30vh] md:max-h-[45vh] lg:max-h-[40vh] xl:max-h-[55vh]">
+      
+         {
+          isComment === false ? <div className="overflow-x-auto overflow-auto min-h-[30vh] md:max-h-[45vh] lg:max-h-[40vh] xl:max-h-[55vh] ">
           {/* new added */}
-          {/* <div className="w-full my-3 flex justify-center">
-            {showSubTask?.length === 0 && addIcon === "sub-task" ? (
-              <div className="mx-auto">
-                <p className="text-center text-2xl font-bold my-5">Empty</p>
-              </div>
-            ) : (
-              taskList?.length === 0 && (
-                <div className="mx-auto">
-                  <p className="text-center text-2xl font-bold my-5">Empty</p>
-                </div>
-              )
-            )}
-          </div> */}
+         
           <table className="table">
-            {/* <table
-            className={`${
-              showSubTask?.length === 0 && addIcon === "sub-task"
-                ? "hidden"
-                : taskList?.length === 0 && "hidden"
-            } table`}
-          > */}
+           
             {/* head */}
             <thead>
               <tr className="font-bold">
@@ -536,14 +349,179 @@ const ProjectTaskPage = () => {
                       onClick={() => handleComment(info.id)}
                       className="font-bold text-white rounded-md px-3 py-1 bg-teal-500"
                     >
-                      Comments
+                      <span onClick={() => setisComment(true)}>Comments</span>
                     </button>
                   </td>
                 </tr>
               </tbody>
             ))}
           </table>
-        </div>
+        </div> :
+
+<div className="flex">
+<div className="overflow-x-auto overflow-auto min-h-[30vh] md:max-h-[45vh] lg:max-h-[40vh] xl:max-h-[55vh] w-[1100px]">
+ {/* new added */}
+
+ <table className="table">
+  
+   {/* head */}
+   <thead>
+     <tr className="font-bold">
+       <th>*</th>
+       <th className="font-bold">
+         {addIcon === "task" ? "Task Title" : "Sub-Task Title"}
+       </th>
+       <th className="font-bold">Email</th>
+       {addIcon === "sub-task" && (
+         <th className="font-bold">Task Title</th>
+       )}
+       <th className="font-bold">Deadline</th>
+       <th className="font-bold">Requirements</th>
+       <th className="font-bold">Description</th>
+       <th className="font-bold">Update</th>
+       <th className="font-bold">Status</th>
+       <th className="font-bold">Comments</th>
+     </tr>
+   </thead>
+   {(showSubTask.length >= 0 && addIcon !== "task"
+     ? showSubTask
+     : taskList
+   )?.map((info, index) => (
+     <tbody key={index}>
+       <tr className="font-medium">
+         <th>{index + 1}</th>
+         <td>{info?.title}</td>
+         <td>
+           <div className="flex gap-1 items-center">
+             <MdOutlineMarkEmailUnread size={20} />
+             {info?.developer_email}
+           </div>
+         </td>
+         {addIcon === "sub-task" && <td>{info?.task_title}</td>}
+         <td>{info?.deadline}</td>
+         <td>{info?.requirements}</td>
+         <td>{info?.description}</td>
+         <td>
+           <div className="">
+             {addIcon === "task" ? (
+               <button
+                 onClick={() => handleUpdateTask(info?.id)}
+                 className="font-bold text-white rounded-md px-3 py-1 bg-blue-600"
+               >
+                 <span onChange={() => setUpdateId(info.id)}>
+                   Update
+                 </span>
+               </button>
+             ) : (
+               <button
+                 onClick={() => handleUpdateSubTask(info?.id)}
+                 className="font-bold text-white rounded-md px-3 py-1 bg-blue-600"
+               >
+                 Update
+               </button>
+             )}
+             <p className="text-xs text-center text-red-400">
+               Please double click
+             </p>
+           </div>
+         </td>
+         <td>
+           {addIcon === "task" && (
+             <div className="">
+               {info?.is_complete === false &&
+               info?.is_active === true ? (
+                 <button
+                   onClick={() => handleMarkStatus(info?.id)}
+                   className="font-bold text-white rounded-md px-3 py-1 bg-pink-500"
+                 >
+                   On Going
+                 </button>
+               ) : info?.is_complete === true &&
+                 info?.is_active === true ? (
+                 <button className="font-bold text-white rounded-md px-3 py-1 bg-red-500">
+                   Lately
+                 </button>
+               ) : info?.is_complete === true &&
+                 info?.is_active === false ? (
+                 <button className="font-bold text-white rounded-md px-3 py-1 bg-green-700">
+                   Done
+                 </button>
+               ) : (
+                 ""
+               )}
+             </div>
+           )}
+           {addIcon === "sub-task" && (
+             <div>
+               {info?.is_complete === true ? (
+                 <button className="font-bold text-white rounded-md px-3 py-1 bg-green-800">
+                   Done
+                 </button>
+               ) : (
+                 <button
+                   onClick={() => handleMarkStatus(info?.id)}
+                   className="font-bold text-white rounded-md px-3 py-1 bg-pink-500"
+                 >
+                   On Going
+                 </button>
+               )}
+             </div>
+           )}
+         </td>
+         <td>
+           <button
+             onClick={() => handleComment(info.id)}
+             className="font-bold text-white rounded-md px-3 py-1 bg-teal-500"
+           >
+             Comments
+           </button>
+         </td>
+       </tr>
+     </tbody>
+   ))}
+ </table>
+</div>
+{/* comment section */}
+<div className="bg-slate-200 w-[350px] overflow-y-auto h-[508px]">
+ <h1 className="text-[25px] font-bold pl-[20px] py-[5px] border-b-2 border-b-gray-500 flex items-center">Comment (3) <span><IoCloseCircleSharp onClick={() => setisComment(false)} className="text-[35px] text-red-400 ml-[110px]"></IoCloseCircleSharp></span></h1>
+ <div className="py-[10px] px-[15px] flex  gap-[10px] border-b-2 border-b-gray-500">
+   <img className="w-[50px] h-[50px] rounded-[50%]" src="https://i.ibb.co/HB28YfJ/istockphoto-1300845620-612x612.jpg" alt="" />
+
+   <div>
+   <h1 className="text-[22px] font-bold">Zisan Islam</h1>
+
+<p className="text-[16px] font-medium text-left">It is a very challenging task... To complete it i gain lot of konwledge and experience... So i am very thankfull.</p>
+   </div>
+ </div>
+
+ <div className="py-[10px] px-[15px] flex  gap-[10px] border-b-2 border-b-gray-500">
+   <img className="w-[50px] h-[50px] rounded-[50%]" src="https://i.ibb.co/HB28YfJ/istockphoto-1300845620-612x612.jpg" alt="" />
+
+   <div>
+   <h1 className="text-[22px] font-bold">Zisan Islam</h1>
+
+<p className="text-[16px] font-medium">It is a very challenging task... To complete it i gain lot of konwledge and experience... So i am very thankfull.</p>
+   </div>
+ </div>
+
+
+ <div className="py-[10px] px-[15px] flex  gap-[10px] border-b-2">
+   <img className="w-[50px] h-[50px] rounded-[50%]" src="https://i.ibb.co/HB28YfJ/istockphoto-1300845620-612x612.jpg" alt="" />
+
+   <div>
+   <h1 className="text-[22px] font-bold">Zisan Islam</h1>
+
+<p className="text-[16px] font-medium">It is a very challenging task... To complete it i gain lot of konwledge and experience... So i am very thankfull.</p>
+   </div>
+ </div>
+
+</div>
+
+</div>
+         }
+
+
+       
         {addIcon === "task" && (
           <div className="w-full mx-auto my-3 flex justify-center">
             <button
